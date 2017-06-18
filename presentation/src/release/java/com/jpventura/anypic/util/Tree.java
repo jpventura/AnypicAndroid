4f75c22face55e1e83206f3c3120fc62bb1f53d3
JP Ventura <contact@jpventura.com>
2017-06-18 08:48:40 -0300

    answer-crashlytics

--- presentation/src/release/java/com/jpventura/anypic/util/Tree.java
+++ presentation/src/release/java/com/jpventura/anypic/util/Tree.java
@@ -20,26 +20,63 @@ import android.app.Application;
 import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.crashlytics.android.answers.Answers;
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.ndk.CrashlyticsNdk;
+
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.crash.FirebaseCrash;
 
 import com.facebook.appevents.AppEventsLogger;
+
+import io.fabric.sdk.android.Fabric;
 
 import timber.log.Timber;
 
 public class Tree extends Timber.Tree {
 
+    private final FirebaseAnalytics mFirebaseAnalytics;
+
     public Tree(@NonNull final Application application) {
         super();
         AppEventsLogger.activateApp(application);
+        Fabric.with(application, new Answers());
+        Fabric.with(application, new Crashlytics(), new CrashlyticsNdk());
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(application);
     }
 
     @Override
     protected void log(int priority, String tag, String message, Throwable throwable) {
+        if (isUserConnected()) {
+            final FirebaseUser user = getUser();
+            Crashlytics.setUserIdentifier(user.getUid());
+            Crashlytics.setUserEmail(user.getEmail());
+            Crashlytics.setUserName(user.getDisplayName());
+        }
+
         switch (priority) {
             case Log.ERROR:
+                Crashlytics.log(priority, tag, message);
+                Crashlytics.logException(throwable);
+
                 FirebaseCrash.logcat(priority, tag, message);
                 FirebaseCrash.report(throwable);
                 break;
+        }
+    }
+
+    private static boolean isUserConnected() {
+        final FirebaseAuth auth = FirebaseAuth.getInstance();
+        return (null != auth) && (auth.getCurrentUser() != null);
+    }
+
+    private static FirebaseUser getUser() throws IllegalStateException {
+        try {
+            return FirebaseAuth.getInstance().getCurrentUser();
+        } catch (NullPointerException e) {
+            throw new IllegalStateException(e);
         }
     }
 
