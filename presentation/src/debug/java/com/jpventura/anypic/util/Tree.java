48f0dff9f62586cf4eef41dc0c527d7b2567bd31
JP Ventura <contact@jpventura.com>
2017-06-20 12:15:48 -0300

    :sparkles: Facebook Stetho debug bridge

--- presentation/src/debug/java/com/jpventura/anypic/util/Tree.java
+++ presentation/src/debug/java/com/jpventura/anypic/util/Tree.java
@@ -18,16 +18,42 @@ package com.jpventura.anypic.util;
 
 import android.app.Application;
 import android.support.annotation.NonNull;
+import android.util.Log;
+import android.util.SparseArray;
 
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.stetho.inspector.console.CLog;
+import com.facebook.stetho.inspector.protocol.module.Console.MessageLevel;
+import com.facebook.stetho.inspector.protocol.module.Console.MessageSource;
 
 import timber.log.Timber;
 
 public class Tree extends Timber.DebugTree {
 
+    private static final SparseArray<MessageLevel> MESSAGE_LEVEL_BY_PRIORITY;
+    static {
+        MESSAGE_LEVEL_BY_PRIORITY = new SparseArray<>();
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.ASSERT, MessageLevel.ERROR);
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.DEBUG, MessageLevel.DEBUG);
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.ERROR, MessageLevel.ERROR);
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.INFO, MessageLevel.LOG);
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.VERBOSE, MessageLevel.DEBUG);
+        MESSAGE_LEVEL_BY_PRIORITY.put(Log.WARN, MessageLevel.WARNING);
+    }
+
     public Tree(@NonNull final Application application) {
         super();
         AppEventsLogger.activateApp(application);
+    }
+
+    @Override
+    protected void log(int priority, String tag, String message, Throwable throwable) {
+        super.log(priority, tag, message, throwable);
+        console(priority, message);
+    }
+
+    private void console(int priority, String message) {
+        CLog.writeToConsole(MESSAGE_LEVEL_BY_PRIORITY.get(priority), MessageSource.OTHER, message);
     }
 
 }
